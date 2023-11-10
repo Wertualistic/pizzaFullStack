a667dafc37ba0f8bc3b4fce8df4dd993d38d5b7d
Wertualistic <abduxalilovmuhammadali1@gmail.com>
2023-11-10 09:50:24 +0500

    First

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import React from "react";
+import Sidebar from "./components/Sidebar/Sidebar.jsx";
+
 const App = () => {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
-  )
-}
+    <>
+      <Sidebar />
+    </>
+  );
+};
 
-export default App
+export default App;
