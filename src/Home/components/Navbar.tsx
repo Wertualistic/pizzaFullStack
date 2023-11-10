bfc06b8f3c60ca152528e4badcd832c107775c48
Khojiakbarbe <xojiakbarnosiraliyev1@gmail.com>
2023-11-10 10:35:33 +0500

    changed

--- src/Home/components/Navbar.tsx
+++ src/Home/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar: React.FC = () => {
         <div className="flex justify-between items-center bg-transparent text-white">
             <div>
                 <h3 className="text-xl">Jaegar Resto</h3>
-                <p>Tuesday, 2 Feb 2021</p>
+                <p>Tuesday, 2 Feb 2021 test</p>
             </div>
             <div>
                 <input type="search" className="p-3 rounded-lg bg-[#393C49] text-white" placeholder="Search for food, coffe, etc.." />
